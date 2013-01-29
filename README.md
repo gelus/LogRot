b0667fced3e4478035ad3db3415e4014aa4eb203
Gelus <millervox@gmail.com>
2013-01-28 18:23:43 -0700

    Logrot has been wrapped in a closure.
    Technique updates, more flexible build, runs off two uls rather than
    building a copy of the one (that was a bad choice). Now functions more
    like a library.

--- README.md
+++ README.md
@@ -13,17 +13,25 @@ LogRot has a long way to go before I feel comfortable saying it is a full true-t
 <hr />
 <h2>Use:</h2>
 <h5>HTML:</h5>
-LogRot uses an unordered list that is wrapped in a div.<br />
-\<div\><br />
-\<ul\ id="ID-Of-UL"><br />
-\<li\> Item \</li\><br />
-\<li\> Item \</li\><br />
-\<li\> Item \</li\><br />
-\<li\> Item \</li\><br />
-\</ul\><br />
-\</div\><br />
+LogRot uses two unordered lists that are wrapped in a div.<br />
+<pre>
+	
+	<div id="Your-ID">
+		<ul>
+			<li>item</li>
+			<li>item</li>
+			<li>item</li>
+		</ul>
+		<ul>
+			<li>item</li>
+			<li>item</li>
+			<li>item</li>
+		</ul>
+	</div>
+
+</pre>
 <h5>Call:</h5>
-new logrot("ID-Of-UL", {option:value});
+logrot.create("Your-ID", [{options:object}]);
 
 logrot should be called from a window.onload event and the ID passed as the first parameter should be the id of the UL not the wrapping Div. (strange, yes I'll fix it latter.)
 Currently the only option available is fade which accepts true of false and defaults to false. if true fade will look for img/slider-gradient-left.png and img/slider-gradient-right.png and apply them on the coinciding side.
--- logrot.js
+++ logrot.js
@@ -1,181 +1,212 @@
-﻿// JavaScript Document
-function logrot(id, option){
-	
-	//option defaults
-	this.option = (option==undefined)? {}:option;
-	this.option.fade = (this.option.fade==undefined)? false:this.option.fade;
-	
-	
-	var self = this;
-	this.speed = -1;
-	this.orgspeed = this.speed;
-	
-	this.ul = document.getElementById(id);
-		this.box = this.ul.parentNode;
-			this.box.style.position = "relative";
-			this.box.style.overflow = "hidden";		
-			//close parentNode setup	
-		this.ul.style.position = "absolute";		
-		this.ul.style.left = "0px";
-		this.ul.style.width = this.box.offsetWidth+"px";
-		this.ul.style.height = "10px";
-		this.ul.style.listStyle = "none";
-		this.ul.style.padding = "0";
-		this.ul.style.margin = "0";
-		//close setup for ul
-		var exWidth = 0;
-	this.items = this.ul.getElementsByTagName("li");
-		for(var i = 0; i < this.items.length; i++){
-			var istyle = this.items[i].style;
-			istyle.display = "inline-block";
-			istyle.padding = 0;
-			istyle.styleFloat = "left";
-			istyle.cssFloat = "left";
-			istyle.marginLeft = 0;
-			istyle.width = (Math.floor(this.ul.offsetWidth/this.items.length)>this.items[i].offsetWidth)? 
-											Math.floor(this.ul.offsetWidth/this.items.length)+"px":
-											this.items[i].offsetWidth+"px";
-			istyle.textAlign = "center";
-			
-			exWidth += this.items[i].offsetWidth;
-			
-			}//close setup for each item
-		this.ul.style.width = exWidth+20+"px";
-		for(var i = 0; i < this.items.length; i++){
-			for(var x = 0; x < this.items.length; x++){
-				this.box.style.height = parseInt(this.ul.style.height)+"px";
-				this.ul.style.height = (this.items[x].offsetHeight > parseInt(this.ul.style.height)  || x == 0)? this.items[x].offsetHeight+"px":this.ul.style.height;
-				}
-			this.items[i].style.marginTop = (parseInt(this.ul.style.height)/2 - this.items[i].offsetHeight/2) + "px";
-			}//for to align horizontally
-			//close Itemsetup
-	//make copy
-	this.ul2 = this.ul.cloneNode(true);
-		this.ul2.setAttribute('id', this.ul.id+"2")
-		this.items2 = this.ul2.getElementsByTagName("li");
-		this.box.appendChild(this.ul2);
-		this.ul2.style.left = (this.speed < 0)? this.ul.offsetLeft+this.ul.offsetWidth+"px":-this.ul.offsetWidth+"px";
-	
-	this.move = function(){
-		var newleft = parseInt(self.ul.style.left)+self.speed;
-		var newleft2 = parseInt(self.ul2.style.left)+self.speed;
-		if(self.speed < 0){
-			if(newleft2+this.ul2.offsetWidth <= 0 || newleft+this.ul.offsetWidth <= 0){
-				newleft = 0;
-				self.ul.style.left = newleft+"px";
-				self.ul2.style.left = newleft+self.ul.offsetWidth+"px";
-				return;
-				}
-			}
-		else if(self.speed > 0){
-			if(newleft2 >= this.ul2.offsetWidth || newleft >= this.ul.offsetWidth){
-				newleft = 0;
-				self.ul.style.left = newleft+"px";
-				self.ul2.style.left = newleft-self.ul.offsetWidth+"px";
-				return;
-				}
-			}
-		self.ul.style.left = newleft+"px";
-		self.ul2.style.left = newleft2+"px";
-		}
+﻿//logo Rotator
+(function logrot(logrot){
+	logrot.create = function(id, option){
+
+		//option defaults
+		this.option = (option==undefined)? {}:option;
+		this.option.fade = (this.option.fade==undefined)? false:this.option.fade;
+
+		var elm  = document.getElementById(id),
+				ul0  = elm.getElementsByTagName('ul')[0],
+				ul1  = elm.getElementsByTagName('ul')[1],
+				ops  = option,
+				self = this,
+				id   = "#"+id,
+				biggestItemHeight = 0,
+				widths = [0, 0],
+				uls = elm.getElementsByTagName('ul'),
+				speedCalc,
+
+				startTime = Date.now(),
+				lastFrame = startTime,
+				elapsedTime = 0,
+				fps = 40,
+				speed = 75,
+				defaultSpeed = speed;
 		
-	this.ticker = function(){
-		requestAnimationFrame(self.ticker);
-		self.move();
-		}
-	this.ticker();
-	
-	this.getScrollTop = function(){
-			if(typeof pageYOffset!= 'undefined'){
-					//most browsers
-					return pageYOffset;
-			}
-			else{
-					var B= document.body; //IE 'quirks'
-					var D= document.documentElement; //IE with doctype
-			D= (D.clientHeight)? D: B;
-					return D.scrollTop;
-			}
-	}
-	
-	this.slowing = function(){
-		
-		if(parseInt(self.speed) > self.orgspeed){self.speedCalc -= .1;}
-		else if(parseInt(self.speed) < self.orgspeed){self.speedCalc += .1}
-		self.speed = Math.floor(self.speedCalc);
-		if (Math.floor(self.speedCalc) == self.orgspeed)self.box.slowing = false;
-		if(self.box.moused == false && self.box.slowing == true){requestAnimationFrame(self.slowing);}
-		}
-	this.noMousey = function(e){
-		var height = this.box.offsetHeight, top = this.box.offsetTop-self.getScrollTop(), bottom = top+height;
-		var width = this.box.offsetWidth, left = this.box.offsetLeft, right = left+width;
-		var Xmousepos = e.clientX, Ymousepos = e.clientY;
-		
-		if(Xmousepos<left+10||Xmousepos>right-10||Ymousepos<top+3||Ymousepos>bottom-3){
-			self.box.moused = false;
-			self.box.slowing = true;
-			self.speedCalc = self.speed;
-			self.slowing();
+
+		ul0.className = "logul0";
+		ul1.className = "logul1";
+
+		addCssRule(id+' ul li', 'float: left;margin:0;padding:0;');
+
+		for(var i = 0, len = uls.length; i<len; i++){
+			var lis = uls[i].getElementsByTagName('li');
+			for(var x = 0, leng = lis.length;x<leng;x++){
+				//window.node = lis[x];
+				widths[i] += lis[x].offsetWidth;
+				console.log(lis[x].offsetHeight, biggestItemHeight, lis[x].id);
+				if(lis[x].offsetHeight > biggestItemHeight)biggestItemHeight = lis[x].offsetHeight;
 			}
 		}
-	this.mousey = function(e){
-		self.box.moused = true;
-		self.box.slowing = false;
-		var width = this.box.offsetWidth, left = this.box.offsetLeft, right = left+width;
-		var XmousePos = e.clientX, XmouseRelPos = (XmousePos - left);
-		prec = parseInt(((XmouseRelPos)/width*100)-50);
-		this.speed = (prec/5 >= 0)? Math.ceil(prec/5):Math.floor(prec/5);
-		if(this.speed === 0)this.speed++;
-		}
-	//events:
+		console.log(biggestItemHeight);
+
+
+		addCssRule(id, "height: "+biggestItemHeight+"px; overflow: hidden;");
+		addCssRule(id+' ul li', "height: "+biggestItemHeight+"px");
+		addCssRule(
+			id+" ul",
+			"position: absolute;height: auto;list-style: none;padding: 0;margin: 0;"
+			);
+		addCssRule(".logul0", "width: "+widths[0]+"px; left: 0px");
+		addCssRule(".logul1", "width: "+widths[1]+"px; left: "+widths[0]+"px");
+
 		if(window.addEventListener){
-			this.box.addEventListener("mousemove", function(event){self.mousey(event)}, false);
-			this.box.addEventListener("mouseout", function(event){self.noMousey(event);}, false);
-			}
-		else if(window.attachEvent){
-			this.box.attachEvent("onmousemove", function(event){self.mousey(event)});
-			this.box.attachEvent("onmouseout", function(){self.noMousey(event);})
-			}
-			
-	//options---
-	if(this.option.fade){
-		var fadeboxLeft = document.createElement('div');
-			fadeboxLeft.setAttribute('style', 'display:block;position: absolute;height: 100%;width: 130px;left:0;top:0;z-index:100;background: url(img/slider-gradient-left.png) repeat-y top left');
-		var fadeboxRight = document.createElement('div');
-			fadeboxRight.setAttribute('style', 'display:block;position: absolute;height: 100%;width: 130px;right:0;top:0;z-index:100;background: url(img/slider-gradient-right.png) repeat-y top right;');
-		this.box.appendChild(fadeboxLeft);
-		this.box.appendChild(fadeboxRight);
+			elm.addEventListener("mousemove", mouseMoveHandle, false);
+			elm.addEventListener("mouseout", mouseOutHandle, false);
 		}
+		else if(window.attachEvent){
+			elm.attachEvent("onmousemove", mouseMoveHandle);
+			elm.attachEvent("onmouseout", mouseOutHandle);
+		}
+
+		function mouseMoveHandle(e){
+			elm.moused = true;
+			elm.slowing = false;
+			var width = elm.offsetWidth, left = getLeft(elm), right = left+width;
+			var XmousePos = e.clientX, XmouseRelPos = (XmousePos - left);
+			prec = parseInt(((XmouseRelPos)/width*100)-50);
+			speed = Math.ceil(defaultSpeed*(prec/10));
+
+			if(speed >= 0 && speed<40)speed = 40;
+			if(speed <0 && speed > -40)speed = -40;
+		}
+
+		function mouseOutHandle(e){
+			var height = elm.offsetHeight, top = elm.offsetTop-getScrollTop(), bottom = top+height;
+			var width = elm.offsetWidth, left = getLeft(elm), right = left+width;
+			var Xmousepos = e.clientX, Ymousepos = e.clientY;
 		
-	}//close logrot constructor
-	
-	
-	
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-// requestAnimationFrame polyfill by Erik Möller
-// fixes from Paul Irish and Tino Zijdel
-(function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
-|| window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
- 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
- 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
+			if(Xmousepos<left||Xmousepos>right-10||Ymousepos<top+3||Ymousepos>bottom-3){
+				elm.moused = false;
+				elm.slowing = true;
+				speedCalc = speed;
+				slowing();
+			}
+		}
+
+		function slowing(){
+			if(speed > defaultSpeed){speedCalc -= 3;}
+			else if(speed < defaultSpeed){speedCalc += 3}
+			speed = speedCalc;
+			if (Math.floor(self.speedCalc) == self.orgspeed)self.box.slowing = false;
+			if(elm.moused == false && elm.slowing == true){
+				if(speed <= defaultSpeed+2 && speed >= defaultSpeed){
+					speed = defaultSpeed;
+					return;
+				}
+				requestFrame(slowing);
+			}
+		}
+
+		if(this.option.fade){
+			addCssRule(id+" .fadeboxLeft", 'display:block;position: absolute;height: 100%;width: 50%;left:-1px;top:0;z-index:100;background: url('+link+'indexSupport/images/slider-gradient-left.png) repeat-y top left');
+			addCssRule(id+" .fadeboxRight", 'display:block;position: absolute;height: 100%;width: 50%;right:-1px;top:0;z-index:100;background: url('+link+'indexSupport/images/slider-gradient-right.png) repeat-y top right;');
+			var fadeboxLeft = document.createElement('div');
+				fadeboxLeft.className = "fadeboxLeft";
+			var fadeboxRight = document.createElement('div');
+				fadeboxRight.className = "fadeboxRight"
+			elm.appendChild(fadeboxLeft);
+			elm.appendChild(fadeboxRight);
+		}
+
+		function loop(){
+			var time = Date.now();
+			elapsedTime = time-lastFrame;
+
+			if(time >= lastFrame+(1000/fps)){
+				lastFrame = Date.now();
+				update();
+			}
+			requestFrame(loop);
+		}
+
+		function update(){
+			if(ul0.style.left == "")ul0.style.left = 0+"px";
+			if(ul1.style.left == "")ul1.style.left = widths[0]+"px";
+			delta = elapsedTime/1000;
+			var move = (speed * delta);
+			
+
+			ul0.style.left = Math.floor(parseInt(ul0.style.left) - move)+"px";
+			ul1.style.left = Math.floor(parseInt(ul1.style.left) - move)+"px";
+
+			rearangeIt();
+			
+		}
+
+		loop();
+
+		function rearangeIt(){
+			if(speed < 0){
+				if(parseInt(ul0.style.left)>elm.offsetWidth)ul0.style.left = (parseInt(ul1.style.left)-widths[0])+"px"
+				if(parseInt(ul1.style.left)>elm.offsetWidth)ul1.style.left = (parseInt(ul0.style.left)-widths[1])+"px"
+				return
+			}
+			else if(speed > 0){
+				if(parseInt(ul0.style.left)<-widths[0])ul0.style.left = (parseInt(ul1.style.left)+widths[1])+"px"
+				if(parseInt(ul1.style.left)<-widths[1])ul1.style.left = (parseInt(ul0.style.left)+widths[0])+"px"
+				return;
+			}
+		}
+	}// close create logrot
+	function getScrollTop(){
+		if(typeof pageYOffset!= 'undefined'){
+			//most browsers
+			return pageYOffset;
+		}
+		else{
+			var B= document.body; //IE 'quirks'
+			var D= document.documentElement; //IE with doctype
+			D= (D.clientHeight)? D: B;
+			return D.scrollTop;
+		}
+	}
+	function getLeft(elm){
+		var parent = elm.offsetParent,
+				left = elm.offsetLeft;
+
+		while(parent !== document.body && parent !== document.getElementsByTagName('html')[0]){
+			left += parent.offsetLeft;
+			parent = parent.offsetParent;
+		}
+		return left;
+	}
+
+	function requestFrame(callback){
+		var x = window.requestAnimationFrame       || 
+		        window.webkitRequestAnimationFrame || 
+		        window.mozRequestAnimationFrame    || 
+		        window.oRequestAnimationFrame      || 
+		        window.msRequestAnimationFrame     || 
+		        function( callback ){
+		          window.setTimeout(callback, 1000 / 60);
+		        };
+		x(callback);
+	}
+	function addCssRule(selector, rule) {
+	  if (document.styleSheets){
+	    var head = document.getElementsByTagName('head')[0];
+	    head.appendChild(document.createElement('style'));
+
+	    var i = document.styleSheets.length-1;
+	    var ss = document.styleSheets[i];
+
+	    var l=0;
+	    if (ss.cssRules) {
+	      l = ss.cssRules.length;
+	    } else if (ss.rules) {
+	      // IE
+	      l = ss.rules.length;
+	    }
+
+	    if (ss.insertRule) {
+	      ss.insertRule(selector + ' {' + rule + '}', 0);
+	    } else if (ss.addRule) {
+	      // IE
+	      ss.addRule(selector, rule, l);
+	    }
+	  }
+	};
+})(window.logrot = window.logrot||{});
+//close logo Rotator
